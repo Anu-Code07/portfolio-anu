33a40cdfc421f5c7b05b6685f841d7f93d5b39f3
Anu-Code07 <anurag.kr.singh07@gmail.com>
2023-04-23 21:48:07 +0530

    todo list

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -20,80 +20,104 @@ function Projects() {
   const url = useLocation();
   const pathname = url.pathname;
   return (
- 
-      <Box className={pathname === "/projects" ? "p-bg" : "jump"}>
-        {pathname === "/projects" ? <NavBar /> : ""}
-        <Box className="project-heading">
-          <Typography variant="h2" sx={{}}>
-            Projects
-          </Typography>
-          <Divider
-            sx={{
-              height: "4px",
-              backgroundColor: "#61bffb",
-              width: "20%",
-              ml: 3,
-              mt: 1,
-            }}
-          />
-        </Box>
-        <Grid
-          container
-          spacing={1}
+    <Box className={pathname === "/projects" ? "p-bg" : "jump"}>
+      {pathname === "/projects" ? <NavBar /> : ""}
+      <Box className="project-heading">
+        <Typography variant="h2" sx={{}}>
+          Projects
+        </Typography>
+        <Divider
           sx={{
-            padding: "30px",
+            height: "4px",
+            backgroundColor: "#61bffb",
+            width: "20%",
+            ml: 3,
+            mt: 1,
           }}
-          rowGap={2}
-          className="Cardcontainer"
-        >
-          <Grid item xs={12} md={6} lg={4}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardMedia
-                component="img"
-                height="140"
-                image={Movie}
-                alt="movie"
-              />
-              <CardContent>
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                  sx={{
-                    fontWeight: "bold",
-                  }}
-                >
-                  Movie app
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Search and save your favourite movies and even find your
-                  favourite actor
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small" onClick={() => {}} variant="text">
-                  <Link
-                    href="https://anu-code07.github.io/Movie-app/#/"
-                    target={"_blank"}
-                  >
-                    {" "}
-                    Live Demo
-                  </Link>
-                </Button>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-         
-         
-        </Grid>
-        <Box
-          sx={{
-            height: "200px",
-          }}
-        ></Box>
+        />
       </Box>
-   
+      <Grid
+        container
+        spacing={1}
+        sx={{
+          padding: "30px",
+        }}
+        rowGap={2}
+        className="Cardcontainer"
+      >
+        <Grid item xs={12} md={6} lg={4}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia component="img" height="140" image={Movie} alt="movie" />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                sx={{
+                  fontWeight: "bold",
+                }}
+              >
+                Movie app
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Search and save your favourite movies and even find your
+                favourite actor
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" onClick={() => {}} variant="text">
+                <Link
+                  href="https://anu-code07.github.io/Movie-app/#/"
+                  target={"_blank"}
+                >
+                  {" "}
+                  Live Demo
+                </Link>
+              </Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia component="img" height="140" image={Movie} alt="movie" />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                sx={{
+                  fontWeight: "bold",
+                }}
+              >
+                Todo List
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                What's special about a todo list you ask? Well its made with
+                <b> Recoil.js</b>!, a global statemanagement libary
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" onClick={() => {}} variant="text">
+                <Link
+                  href="https://todo-peach-eta.vercel.app/"
+                  target={"_blank"}
+                >
+                  {" "}
+                  Live Demo
+                </Link>
+              </Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+      <Box
+        sx={{
+          height: "200px",
+        }}
+      ></Box>
+    </Box>
   );
 }
 

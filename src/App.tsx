15b06a1a367f7058a9746d6a42443d5584fc1cf2
Anu-Code07 <anurag.kr.singh07@gmail.com>
2023-05-06 23:52:19 +0530

    removed framer

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import NavRoute from "../src/Components/NavRoutes";
 import "./App.css";
 import About from "./Components/About";
 import Home from "./Components/Home";
-import { AnimatePresence } from "framer-motion";
+
 import { useLocation } from "react-router-dom";
 
 function App() {
@@ -12,9 +12,7 @@ function App() {
   const pathname = url.pathname;
   return (
     <Box className="myapp-container">
-      <AnimatePresence initial={false} mode={"wait"} >
-        <NavRoute key={pathname} />
-      </AnimatePresence>
+      <NavRoute key={pathname} />
     </Box>
   );
 }
